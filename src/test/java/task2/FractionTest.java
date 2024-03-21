9a2d36658c6c1e3e8795ad46594470145973529e
Tim Zolleis <tim@zolleis.net>
2024-03-21 09:55:57 +0100

    test(task2): add tests

---
+++ src/test/java/task2/FractionTest.java
@@ -0,0 +1,50 @@
+package task2;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FractionTest {
+
+    @Test
+    void testSumWith5() {
+        final int n = 5;
+        final Fraction fraction = new Fraction(1, 1);
+        for (int i = 2; i <= n; i++) {
+            System.out.println(i);
+            fraction.add(new Fraction(1, i));
+        }
+        Assertions.assertEquals("137/60", fraction.toString());
+    }
+
+    @Test
+    void testSumWith10() {
+        final int n = 10;
+        final Fraction fraction = new Fraction(1, 1);
+        for (int i = 2; i <= n; i++) {
+            fraction.add(new Fraction(1, i));
+        }
+        Assertions.assertEquals("7381/2520", fraction.toString());
+    }
+
+    @Test
+    void testMultiply() {
+        final Fraction fraction = new Fraction(1, 2);
+        fraction.multiply(new Fraction(2, 3));
+        Assertions.assertEquals("1/3", fraction.toString());
+    }
+
+    @Test
+    void testDivide() {
+        final Fraction fraction = new Fraction(1, 2);
+        fraction.divide(new Fraction(2, 3));
+        Assertions.assertEquals("3/4", fraction.toString());
+    }
+
+
+    @Test
+    void testEuler() {
+        final Fraction fractionWith5 = EulerCalculator.euler(5);
+        Assertions.assertEquals("163/60", fractionWith5.toString());
+    }
+
+}
